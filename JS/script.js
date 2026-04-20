acd41cd160a8a639fb553f9c7745c06eb893983c
cheesymuffin8 <107362502+cheesymuffin8@users.noreply.github.com>
2026-04-20 17:22:23 -0400

    carrés

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,12 @@
 let posX = 200;
 let vitesseX = 7.5;
 
+let row = 0
+let SPosX = 300
+let SPosY = 200
 
 function setup() {
-    createCanvas(800, 600);
+    createCanvas(800, 600, document.querySelector("body"));
 
 }
 
@@ -13,13 +16,24 @@ function draw() {
     noStroke();
     circle(posX, posY, 50);
 
-    if (posX > 800 - 25 || posX < 0 + 25){
+    if (posX > 800 - 25 || posX < 0 + 25) {
         vitesseX = -vitesseX;
     }
-    if (posY > 600 - 25 || posY < 0 +25){
-       vitesseY = -vitesseY; 
+    if (posY > 600 - 25 || posY < 0 + 25) {
+        vitesseY = -vitesseY;
     }
 
     posX += vitesseX;
     posY += vitesseY;
+
+    function recurs() {
+        if (row < 11) {
+            for (let i = 0; i < 11; i++) {
+                square(SPosX + (i * 5), SPosY + (row * 5), 5)
+            }
+            row++
+            recurs()
+        }
+    }
+
 }
