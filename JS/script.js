637b87bf52696057d4b84cf3ddd711b73b348318
MaxDuro <Maxime_08duro@hotmail.com>
2026-04-27 20:41:33 -0400

    Ajout  de saut, accroupissement, de gravité et de friction pour donné de la fluidité dans les sauts et les mouvements

--- JS/script.js
+++ JS/script.js
@@ -1,72 +1,171 @@
-let height = 600
-let width = 800
-let floorHeight = height - 250
-
-let plrs = [
-    // Info joueur #1
-    {
-        vie : 100,
-        energie : 100,
-        vitesseX : 0,
-        vitesseY : 0,
-        posX : 650, 
-        posY : floorHeight,
-        crouching: false,
-        jumping : false,
-    },
-    
-    // Info joueur #2
-    {
-        vie : 100,
-        energie : 100,
-        vitesseX : 0,
-        vitesseY : 0,
-        posX : 150, 
-        posY : floorHeight,
-        crouching : false,
-        jumping : false,
-    },
-]
+let height = 600;
+let width = 800;
+let floorHeight = height - 100;
 
 let hitboxSize = 50;
 
-let mainContainer = document.getElementById("mainContainer")
+// infos Joueur 1
+let plrs = [
+    {
+        vie: 100,
+        energie: 100,
+        posX: 650,
+        posY: floorHeight,
+        vitesseX: 0,
+        vitesseY: 0,
+        speed: 0.5,
+        jumpForce: -12,
+        gravity: 0.6,
+        friction: 0.85,
+        crouching: false,
+        jumping: false,
+    },
+
+// infos Joueur 2
+    {
+        vie: 100,
+        energie: 100,
+        posX: 150,
+        posY: floorHeight,
+        vitesseX: 0,
+        vitesseY: 0,
+        speed: 0.5,
+        jumpForce: -12,
+        gravity: 0.6,
+        friction: 0.85,
+        crouching: false,
+        jumping: false,
+    }
+];
+
+let mainContainer = document.getElementById("mainContainer");
 
 function setup() {
-    let canvas = createCanvas(width, height)
-    canvas.parent(mainContainer)
+    let canvas = createCanvas(width, height);
+    canvas.parent(mainContainer);
 }
 
-function draw(){
-    background(0);
+function draw() {
+    background(30);
+    drawFloor();
+    updatePlayer1();
+    updatePlayer2();
     drawPlayer1();
     drawPlayer2();
 }
 
-function drawPlayer1(){  
-    
-    if(keyIsDown(37) && plrs[0].posX > hitboxSize/2){
-        plrs[0].posX -= 5;
-    }
-
-    if(keyIsDown(39) && plrs[0].posX < width - (hitboxSize/2)) {
-        plrs[0].posX += 5;
-    }
-
-    fill(0,0,255)
-    ellipse(plrs[0].posX, plrs[0].posY, hitboxSize, hitboxSize)
+//Ligne du sol
+function drawFloor() {
+    fill(100);
+    rect(0, floorHeight + hitboxSize / 2, width, 10);
 }
 
-function drawPlayer2(){
+//Joueur 1
+function updatePlayer1() {
 
-        if(keyIsDown(65) && plrs[1].posX > hitboxSize/2){
-        plrs[1].posX -= 5;
+    // Gauche / Droite
+    if (keyIsDown(37)) {
+        plrs[0].vitesseX -= plrs[0].speed;
+    }
+    if (keyIsDown(39)) {
+        plrs[0].vitesseX += plrs[0].speed;
     }
 
-    if(keyIsDown(68) && plrs[1].posX < width - (hitboxSize/2)) {
-        plrs[1].posX += 5;
+    // Friction
+    plrs[0].vitesseX *= plrs[0].friction;
+
+    // Appliquer mouvement
+    plrs[0].posX += plrs[0].vitesseX;
+
+    // Saut
+    if (keyIsDown(38) && !plrs[0].jumping) {
+        plrs[0].vitesseY = plrs[0].jumpForce;
+        plrs[0].jumping = true;
     }
-    
-    fill(255,0,0)
-    ellipse(plrs[1].posX, plrs[1].posY, hitboxSize, hitboxSize);
+
+    // Gravité
+    plrs[0].vitesseY += plrs[0].gravity;
+    plrs[0].posY += plrs[0].vitesseY;
+
+    // Collision sol
+    if (plrs[0].posY >= floorHeight) {
+        plrs[0].posY = floorHeight;
+        plrs[0].vitesseY = 0;
+        plrs[0].jumping = false;
+    }
+
+    // Accroupissement
+    if (keyIsDown(40)) {
+        plrs[0].crouching = true;
+    } else {
+        plrs[0].crouching = false;
+    }
+
+    // Limites écran
+    plrs[0].posX = constrain(plrs[0].posX, hitboxSize / 2, width - hitboxSize / 2);
+}
+
+function drawPlayer1() {
+    fill(0, 0, 255);
+
+    if (plrs[0].crouching) {
+        ellipse(plrs[0].posX, plrs[0].posY + 10, hitboxSize, hitboxSize / 2);
+    } else {
+        ellipse(plrs[0].posX, plrs[0].posY, hitboxSize, hitboxSize);
+    }
+}
+
+//Joueur 2
+function updatePlayer2() {
+
+    // Gauche / Droite (A / D)
+    if (keyIsDown(65)) {
+        plrs[1].vitesseX -= plrs[1].speed;
+    }
+    if (keyIsDown(68)) {
+        plrs[1].vitesseX += plrs[1].speed;
+    }
+
+    // Friction
+    plrs[1].vitesseX *= plrs[1].friction;
+
+    // Mouvement
+    plrs[1].posX += plrs[1].vitesseX;
+
+    // Saut (W)
+    if (keyIsDown(87) && !plrs[1].jumping) {
+        plrs[1].vitesseY = plrs[1].jumpForce;
+        plrs[1].jumping = true;
+    }
+
+    // Gravité
+    plrs[1].vitesseY += plrs[1].gravity;
+    plrs[1].posY += plrs[1].vitesseY;
+
+    // Collision sol
+    if (plrs[1].posY >= floorHeight) {
+        plrs[1].posY = floorHeight;
+        plrs[1].vitesseY = 0;
+        plrs[1].jumping = false;
+    }
+
+    // Accroupissement (S)
+    if (keyIsDown(83)) {
+        plrs[1].crouching = true;
+    } else {
+        plrs[1].crouching = false;
+    }
+
+    // Limites écran
+    plrs[1].posX = constrain(plrs[1].posX, hitboxSize / 2, width - hitboxSize / 2);
+}
+
+function drawPlayer2() {
+    fill(255, 0, 0);
+
+    if (plrs[1].crouching) {
+        ellipse(plrs[1].posX, plrs[1].posY + 10, hitboxSize, hitboxSize / 2);
+    } else {
+        ellipse(plrs[1].posX, plrs[1].posY, hitboxSize, hitboxSize);
+    }
 }
